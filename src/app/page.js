a0fdde2d86f706796035cb21f01318bbf45f3b83
navyline <nattakorn3667@gmail.com>
2024-06-20 13:27:18 +0700

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -58,10 +58,10 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
           <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
+          <h2>
+            Learn More <span>-&gt;</span>
+          </h2>
         </a>
 
         <a
